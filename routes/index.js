bfea61329717cf46f7eaf9b3ebc2864e23795b71
joe19990131 <joe21854946@gmail.com>
2020-05-14 14:48:52 +0800

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get(/(.*)\.(jpg|gif|png|ico|css|js|txt|svg|ttf|eot|woff|map)/i, function(req, res) {
+  res.sendfile(__dirname + "/" + req.params[0] + "." + req.params[1], function(err) {
+      if (err) res.send(404);
+  });
+});
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get(/(.*)\.(jpg|gif|png|ico|css|js|txt|svg|ttf|eot|woff|map)/i, function(req, res) {
+  res.sendfile(__dirname + "/" + req.params[0] + "." + req.params[1], function(err) {
+      if (err) res.send(404);
+  });
+});
+module.exports = router;
