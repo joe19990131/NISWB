4a52ba6395ac03adef517c9fa17eb14d2c7deab8
joe19990131 <joe21854946@gmail.com>
2020-06-04 15:40:26 +0800

    lab grid work

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,56 @@ var router = express.Router();
 var mysql = require('mysql');
 var bodyParser = require('body-parser');
 var app = express();
-/* GET home page. */
-
 
 //sql qetting
+var connStatus = 0;
 var conn = mysql.createConnection({
   host : 'localhost',
   prot : '3306',
   user: 'root',
   password : '123456',
-  database : 'nisdb'
+  database : 'nis'
 });
 //sql setting end
-
+/* GET home page. */
 router.get('/', function(req, res, next) {
   res.location('/')
   res.render('index', { title: 'Express' });
 });
 
+
+
+router.post('/lab',function(req,res,next){
+var nstNo = "12";
+  var sql = "select nst, BNo, CkCon,ckdate "+
+  "from checkrecord join bhdata using(pNo) join bedrecord using(BNo)"+
+  " where nst = '"+nstNo+"' order by Bno;"
+            
+  //console.log(req);
+  console.log("i am SBL");
+
+  if(connStatus == 0){
+    conn.connect(function(err){
+      if(err) throw err;
+      console.log('connect success!');
+      connStatus ++;
+      console.log(connStatus);
+      });
+  }
+  conn.query(sql,function(err,rows){
+    console.log(rows);
+    if(err){
+      console.log(err);
+    }else{
+      res.json(rows);
+      //res.end();
+    }
+  })
+});
 router.get(/(.*)\.(jpg|gif|png|ico|css|js|txt|svg|ttf|eot|woff|map)/i, function(req, res) {
   res.sendfile(__dirname + "/" + req.params[0] + "." + req.params[1], function(err) {
       if (err) res.send(404);
   });
 });
+
 module.exports = router;
