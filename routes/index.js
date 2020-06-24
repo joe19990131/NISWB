a53e9628f77832cff69d151e1d91a9870088a9c7
tt8871720 <32476259+tt8871720@users.noreply.github.com>
2020-06-24 13:15:28 +0800

    edulist

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,11 @@ var app = express();
 //sql qetting
 var connStatus = 0;
 var conn = mysql.createConnection({
-  host : 'localhost',
-  prot : '3306',
+  host: 'localhost',
+  prot: '3306',
   user: 'root',
-  password : '123456',
-  database : 'nis'
+  password: '123456',
+  database: 'nis'
 });
 /*var conn = mysql.createConnection({
   host : 'JS108-36',
@@ -22,72 +22,72 @@ var conn = mysql.createConnection({
 });*/
 //sql setting end
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.location('/')
   res.render('index', { title: 'NIS WhiteBoard' });
-  
+
 });
 
 
-router.post('/getEElist',function(req,res,next){
+router.post('/getEElist', function (req, res, next) {
   var nstNo = req.body['nstNo'];
-  var sql = "select EEName,EENo,DeptCode from eecode WHERE DeptCode = concat('A','"+nstNo+"','0');";
+  var sql = "select EEName,EENo,DeptCode from eecode WHERE DeptCode = concat('A','" + nstNo + "','0');";
   console.log("i am SBL2");
-  if(connStatus == 0){
-    conn.connect(function(err){
-      if(err) throw err;
+  if (connStatus == 0) {
+    conn.connect(function (err) {
+      if (err) throw err;
       console.log('connect success!');
-      connStatus ++;
+      connStatus++;
       console.log(connStatus);
-      });
+    });
   }
-  conn.query(sql,function(err,rows){
+  conn.query(sql, function (err, rows) {
     console.log(rows);
-    if(err){
+    if (err) {
       console.log(err);
-    }else{
+    } else {
       res.jsonp(rows);
       //res.end();
     }
   })
 });
 //document.ready please copy below code
-router.post('/getCKlist',function(req,res,next){
+router.post('/getCKlist', function (req, res, next) {
   var sql = "SELECT distinct CKSort,CKCon from checkrecord;";
   console.log("i am SBL2");
-  conn.query(sql,function(err,rows){
+  conn.query(sql, function (err, rows) {
     console.log(rows);
-    if(err){
+    if (err) {
       console.log(err);
-    }else{
+    } else {
       res.jsonp(rows);
       //res.end();
     }
   })
 });
 
-router.post('/getFNamelist',function(req,res,next){
-  var sql = "SELECT FName,FNo from mdeddata;;";
+router.post('/getFNamelist', function (req, res, next) {
+  var sql = "SELECT FName,FNo from mdeddata;";
   console.log("i am SBL2");
-  conn.query(sql,function(err,rows){
+  conn.query(sql, function (err, rows) {
     console.log(rows);
-    if(err){
+    if (err) {
       console.log(err);
-    }else{
+    } else {
       res.jsonp(rows);
       //res.end();
     }
   })
 });
 
-router.post('/getBedlist',function(req,res,next){
+router.post('/getBedlist', function (req, res, next) {
   var sql = "SELECT BNo,nstname FROM bedrecord where nst = 12;";
   console.log("i am SBL2");
-  conn.query(sql,function(err,rows){
+  conn.query(sql, function (err, rows) {
     //sconsole.log(rows);
-    if(err){
+    if (err) {
       console.log(err);
-    }else{
+    } else {
       res.json(rows);
       console.log(rows);
       //res.end();
@@ -95,45 +95,59 @@ router.post('/getBedlist',function(req,res,next){
   })
 });
 
+router.post('/edu', function (req, res, next){
+  var sql = "SELECT fileURL,FName from mdeddata;"
+  console.log("i am SBL2");
+  conn.query(sql, function (err, rows) {
+    // console.log(ros);
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(rows);
+      console.log(rows);
+      // res.end();
+    }
+  })
+});
 
-router.post('/lab',function(req,res,next){
-var nstNo = req.body['nstNo'];
-var MN = req.body['MN'];
-var lt = req.body['CKSort']
-console.log(MN);
-  var sql = "select nst, BNo, CkCon,CKSort,DATE_FORMAT(ckdate, '%Y / %m / %d') as ckdate "+
-  "from checkrecord join bhdata using(pNo) join bedrecord using(BNo)"+
-  " where "+
-  "(nst = '"+nstNo+"') "+
-  "and (mn = '"+MN+"' or '' = '"+MN+"')"+
-  "and (cksort = '"+lt+"' or '' = '"+lt+"') "+
-  "order by Bno;"
-            
+router.post('/lab', function (req, res, next) {
+  var nstNo = req.body['nstNo'];
+  var MN = req.body['MN'];
+  var lt = req.body['CKSort']
+  console.log(MN);
+  var sql = "select nst, BNo, CkCon,CKSort,DATE_FORMAT(ckdate, '%Y / %m / %d') as ckdate " +
+    "from checkrecord join bhdata using(pNo) join bedrecord using(BNo)" +
+    " where " +
+    "(nst = '" + nstNo + "') " +
+    "and (mn = '" + MN + "' or '' = '" + MN + "')" +
+    "and (cksort = '" + lt + "' or '' = '" + lt + "') " +
+    "order by Bno;"
+
   //console.log(req);
   console.log("i am SBL");
 
-  if(connStatus == 0){
-    conn.connect(function(err){
-      if(err) throw err;
+  if (connStatus == 0) {
+    conn.connect(function (err) {
+      if (err) throw err;
       console.log('connect success!');
-      connStatus ++;
+      connStatus++;
       console.log(connStatus);
-      });
+    });
   }
-  conn.query(sql,function(err,rows){
+  conn.query(sql, function (err, rows) {
     //console.log(rows);
     console.log(req);
-    if(err){
+    if (err) {
       console.log(err);
-    }else{
+    } else {
       res.json(rows);
       //res.end();
     }
   })
 });
-router.get(/(.*)\.(jpg|gif|png|ico|css|js|txt|svg|ttf|eot|woff|map)/i, function(req, res) {
-  res.sendfile(__dirname + "/" + req.params[0] + "." + req.params[1], function(err) {
-      if (err) res.send(404);
+router.get(/(.*)\.(jpg|gif|png|ico|css|js|txt|svg|ttf|eot|woff|map)/i, function (req, res) {
+  res.sendfile(__dirname + "/" + req.params[0] + "." + req.params[1], function (err) {
+    if (err) res.send(404);
   });
 });
 
